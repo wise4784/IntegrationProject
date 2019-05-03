dcb4c42559fea60184412f297a0bc1337afe0a63
Sunggu-Jo <rjadkr@naver.com>
2019-05-03 17:43:52 +0900

    asm4,5

---
+++ asm/sunggu/asm_gio/Debug/include/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../include/gio.asm \
+../include/rti_disable.asm 
+
+OBJS += \
+./include/gio.obj \
+./include/rti_disable.obj 
+
+ASM_DEPS += \
+./include/gio.d \
+./include/rti_disable.d 
+
+OBJS__QUOTED += \
+"include/gio.obj" \
+"include/rti_disable.obj" 
+
+ASM_DEPS__QUOTED += \
+"include/gio.d" \
+"include/rti_disable.d" 
+
+ASM_SRCS__QUOTED += \
+"../include/gio.asm" \
+"../include/rti_disable.asm" 
+
+
