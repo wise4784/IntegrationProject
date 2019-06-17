e09844b672f47f3a11f7560813b85bd1bdc2d174
silenc3502 <gcccompil3r@gmail.com>
2019-06-17 15:53:29 +0900

    Add Multi Lane

---
+++ mercenary/many_lane/CMakeFiles/LaneDetect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaneDetect.dir/src/main.cpp.o"
+  "CMakeFiles/LaneDetect.dir/src/LaneDetect.cpp.o"
+  "LaneDetect.pdb"
+  "LaneDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaneDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
