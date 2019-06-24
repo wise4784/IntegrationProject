b15f705c848e378112b46f624130aef9694d0a95
dongh722 <dongh722@naver.com>
2019-06-25 00:48:18 +0900

    edit pos_pid.h

--- APST/dsp/testcode/init_velo_and_udp/pos_pid.h
+++ APST/dsp/testcode/init_velo_and_udp/pos_pid.h
@@ -7,9 +7,9 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+
 //p 2.63 i 8.4 d 0.0015;
-#endif ENCODER_REF 0.3515625 //(deg/bit) (360/1024)
-#define MOTOR_REF
+#define ENCODER_REF 0.3515625 //(deg/bit) (360/1024)
 #define PC 1.000
 #define IC 0.000
 #define DC 0.000
@@ -17,3 +17,4 @@
 #define PGAIN PC*ENCODER_REF
 #define IGAIN IC*ENCODER_REF
 #define DGAIN DC*ENCODER_REF
+#endif
