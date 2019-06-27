389c9ad171126628190478e1a9a7f1f354d28fdf
silenc3502 <gcccompil3r@gmail.com>
2019-06-27 23:22:11 +0900

    Add Vivado Block Based Sine Wave

---
+++ mercenary/fpga_sine_wave/fpga_sine_wave.srcs/sources_1/bd/fpga_sine_wave/ip/fpga_sine_wave_PmodR2R_0_0/fpga_sine_wave_PmodR2R_0_0_stub.vhdl
@@ -0,0 +1,72 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (lin64) Build 2086221 Fri Dec 15 20:54:30 MST 2017
+-- Date        : Thu Jun 27 19:57:34 2019
+-- Host        : sdr-Samsung-DeskTop-System running 64-bit Ubuntu 16.04.3 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/sdr/project/IntegrationProject/mercenary/fpga_sine_wave/fpga_sine_wave.srcs/sources_1/bd/fpga_sine_wave/ip/fpga_sine_wave_PmodR2R_0_0/fpga_sine_wave_PmodR2R_0_0_stub.vhdl
+-- Design      : fpga_sine_wave_PmodR2R_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fpga_sine_wave_PmodR2R_0_0 is
+  Port ( 
+    AXI_LITE_GPIO_araddr : in STD_LOGIC_VECTOR ( 8 downto 0 );
+    AXI_LITE_GPIO_arready : out STD_LOGIC;
+    AXI_LITE_GPIO_arvalid : in STD_LOGIC;
+    AXI_LITE_GPIO_awaddr : in STD_LOGIC_VECTOR ( 8 downto 0 );
+    AXI_LITE_GPIO_awready : out STD_LOGIC;
+    AXI_LITE_GPIO_awvalid : in STD_LOGIC;
+    AXI_LITE_GPIO_bready : in STD_LOGIC;
+    AXI_LITE_GPIO_bresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    AXI_LITE_GPIO_bvalid : out STD_LOGIC;
+    AXI_LITE_GPIO_rdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    AXI_LITE_GPIO_rready : in STD_LOGIC;
+    AXI_LITE_GPIO_rresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    AXI_LITE_GPIO_rvalid : out STD_LOGIC;
+    AXI_LITE_GPIO_wdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    AXI_LITE_GPIO_wready : out STD_LOGIC;
+    AXI_LITE_GPIO_wstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    AXI_LITE_GPIO_wvalid : in STD_LOGIC;
+    Pmod_out_pin10_i : in STD_LOGIC;
+    Pmod_out_pin10_o : out STD_LOGIC;
+    Pmod_out_pin10_t : out STD_LOGIC;
+    Pmod_out_pin1_i : in STD_LOGIC;
+    Pmod_out_pin1_o : out STD_LOGIC;
+    Pmod_out_pin1_t : out STD_LOGIC;
+    Pmod_out_pin2_i : in STD_LOGIC;
+    Pmod_out_pin2_o : out STD_LOGIC;
+    Pmod_out_pin2_t : out STD_LOGIC;
+    Pmod_out_pin3_i : in STD_LOGIC;
+    Pmod_out_pin3_o : out STD_LOGIC;
+    Pmod_out_pin3_t : out STD_LOGIC;
+    Pmod_out_pin4_i : in STD_LOGIC;
+    Pmod_out_pin4_o : out STD_LOGIC;
+    Pmod_out_pin4_t : out STD_LOGIC;
+    Pmod_out_pin7_i : in STD_LOGIC;
+    Pmod_out_pin7_o : out STD_LOGIC;
+    Pmod_out_pin7_t : out STD_LOGIC;
+    Pmod_out_pin8_i : in STD_LOGIC;
+    Pmod_out_pin8_o : out STD_LOGIC;
+    Pmod_out_pin8_t : out STD_LOGIC;
+    Pmod_out_pin9_i : in STD_LOGIC;
+    Pmod_out_pin9_o : out STD_LOGIC;
+    Pmod_out_pin9_t : out STD_LOGIC;
+    s_axi_aclk : in STD_LOGIC;
+    s_axi_aresetn : in STD_LOGIC
+  );
+
+end fpga_sine_wave_PmodR2R_0_0;
+
+architecture stub of fpga_sine_wave_PmodR2R_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "AXI_LITE_GPIO_araddr[8:0],AXI_LITE_GPIO_arready,AXI_LITE_GPIO_arvalid,AXI_LITE_GPIO_awaddr[8:0],AXI_LITE_GPIO_awready,AXI_LITE_GPIO_awvalid,AXI_LITE_GPIO_bready,AXI_LITE_GPIO_bresp[1:0],AXI_LITE_GPIO_bvalid,AXI_LITE_GPIO_rdata[31:0],AXI_LITE_GPIO_rready,AXI_LITE_GPIO_rresp[1:0],AXI_LITE_GPIO_rvalid,AXI_LITE_GPIO_wdata[31:0],AXI_LITE_GPIO_wready,AXI_LITE_GPIO_wstrb[3:0],AXI_LITE_GPIO_wvalid,Pmod_out_pin10_i,Pmod_out_pin10_o,Pmod_out_pin10_t,Pmod_out_pin1_i,Pmod_out_pin1_o,Pmod_out_pin1_t,Pmod_out_pin2_i,Pmod_out_pin2_o,Pmod_out_pin2_t,Pmod_out_pin3_i,Pmod_out_pin3_o,Pmod_out_pin3_t,Pmod_out_pin4_i,Pmod_out_pin4_o,Pmod_out_pin4_t,Pmod_out_pin7_i,Pmod_out_pin7_o,Pmod_out_pin7_t,Pmod_out_pin8_i,Pmod_out_pin8_o,Pmod_out_pin8_t,Pmod_out_pin9_i,Pmod_out_pin9_o,Pmod_out_pin9_t,s_axi_aclk,s_axi_aresetn";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "PmodR2R,Vivado 2017.4";
+begin
+end;
