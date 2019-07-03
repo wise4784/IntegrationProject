693f53ee0f76b3fc43b6d87d47f1f0a8dd9c819f
dongh722 <dongh722@naver.com>
2019-07-03 18:12:29 +0900

    bl code add

--- APST/dsp/testcode/can_test/vcp_can.c
+++ APST/dsp/testcode/can_test/vcp_can.c
@@ -10,16 +10,17 @@
 
 extern char can_tx_buf[22];
 extern char data_buf[9];
-char dat_buf[22];
-extern char *dev0;
+char *can_dev0 = "/dev/ttyUSB0";
+char *can_dev1 = "/dev/ttyUSB1";
 const int *can_fd;
-pthread_mutex_t can_mutx;
+pthread_mutex_t can_mutx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  can_cond = PTHREAD_COND_INITIALIZER;
 
 void init_can(void)
 {
 	int *tmp_fd =(int *)malloc(sizeof(int));
 	int can_mutx_state;
-	*tmp_fd = serial_config(dev0);
+	*tmp_fd = serial_config(can_dev0);
 	can_fd = tmp_fd;
 
 	can_mutx_state = pthread_mutex_init(&can_mutx,NULL);
@@ -34,9 +35,8 @@ void init_can(void)
 
 void send_ins2fpga(void)
 {
-	char buf[23] = {'x','1',' ',' ','0',' ','0',' ','0',' ','0',' ','0',' ','0',' ','0',' ','4','6','\r','\n',0};
-	write(*can_fd,buf,22);
-	printf("send %s\n",buf);
+	char can_tbuf[23] = {'x','1',' ',' ','0',' ','0',' ','0',' ','0',' ','0',' ','0',' ','0',' ','4','6','\r','\n',0};
+	send_data(*can_fd,can_tbuf,22,0);
 }
 
 void Recieve_fpga_data(void)
@@ -44,23 +44,20 @@ void Recieve_fpga_data(void)
     static unsigned int cnt =0;
     volatile unsigned int error_cnt = 0;
     int can_rv_buf_cnt =0;
+
     send_ins2fpga();
+
 #if 1
-    memset(dat_buf,0,sizeof(dat_buf));
+    memset(data_buf,0,8);
     printf("receive\r\n");
-
-    pthread_mutex_lock(&can_mutx);
-    read(*can_fd,dat_buf,21);
-    pthread_mutex_unlock(&can_mutx);
-
-    printf("error %d\n", error_cnt);
-
-    for(can_rv_buf_cnt =0; can_rv_buf_cnt<21 ; can_rv_buf_cnt++)
+    while((data_buf[0]==0) && (data_buf[2]==0))
     {
-        printf("%c",dat_buf[can_rv_buf_cnt]);
+        recv_data(*can_fd);
     }
-    printf("%d\n\n",cnt);
-    cnt++;
+    for(can_rv_buf_cnt =0; can_rv_buf_cnt<8 ; can_rv_buf_cnt++)
+    {
+        printf("%d",data_buf[can_rv_buf_cnt]);
+    }
 #endif
 }
 
--- APST/mcu/RemoteSystemsTempFiles/192.168.0.147/home/root/cannon/testcode/bluetooth_test/bluetooth.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <setjmp.h>
-
-int fd;
-extern char *dev1
