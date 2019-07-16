85ab5baca08b7d557025f491f111316c84b8318b
arislid <arislid@gmail.com>
2019-07-16 13:29:54 +0900

    final project

---
+++ mixergi/mcu/sine_signal/Debug/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../sine_signal.c 
+
+C_DEPS += \
+./sine_signal.d 
+
+OBJS += \
+./sine_signal.obj 
+
+OBJS__QUOTED += \
+"sine_signal.obj" 
+
+C_DEPS__QUOTED += \
+"sine_signal.d" 
+
+C_SRCS__QUOTED += \
+"../sine_signal.c" 
+
+
---
+++ mixergi/mcu/sine_wave_uart/Debug/sine_wave_uart/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../sine_wave_uart/sine_wave_uart.c 
+
+C_DEPS += \
+./sine_wave_uart/sine_wave_uart.d 
+
+OBJS += \
+./sine_wave_uart/sine_wave_uart.obj 
+
+OBJS__QUOTED += \
+"sine_wave_uart/sine_wave_uart.obj" 
+
+C_DEPS__QUOTED += \
+"sine_wave_uart/sine_wave_uart.d" 
+
+C_SRCS__QUOTED += \
+"../sine_wave_uart/sine_wave_uart.c" 
+
+
